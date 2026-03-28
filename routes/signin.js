210cdce649b226ba50b71a69f0c204f3963fe89c
codewithajoydas@gmail.com <codewithajoydas@gmail.com>
2026-03-28 22:17:57 +0530

    setting page complete

--- routes/signin.js
+++ routes/signin.js
@@ -47,8 +47,8 @@ router.post("/", async (req, res) => {
     res.status(200).cookie("token", token, {
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      sameSite: "lax",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     }).json({
       status: "success",
       data: { token, user: userData },
